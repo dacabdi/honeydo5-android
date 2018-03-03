142ec7f1deebd8e9d5144c0f4675b948f684d01d
dacabdi <dacabdi89@ufl.edu>
2018-03-03 14:19:44 -0500

    FIXED

--- app/src/main/java/com/honeydo5/honeydo/app/AppController.java
+++ app/src/main/java/com/honeydo5/honeydo/app/AppController.java
@@ -11,19 +11,9 @@ import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     public static final String TAG = AppController.class.getSimpleName();
 
     public static final String defaultBaseUrl = "http://api.honeydo5.com";
-=======
-    public static final String TAG = AppController.class
-            .getSimpleName();
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-=======
-    public static final String TAG = AppController.class
-            .getSimpleName();
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
 
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
--- app/src/main/java/com/honeydo5/honeydo/app/LoginScreen.java
+++ app/src/main/java/com/honeydo5/honeydo/app/LoginScreen.java
@@ -1,7 +1,5 @@
 package com.honeydo5.honeydo.app;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 import com.honeydo5.honeydo.R;
 
 import android.os.Bundle;
@@ -19,9 +17,7 @@ import com.android.volley.Response;
 
 import java.util.Map;
 import java.util.HashMap;
-=======
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -32,57 +28,26 @@ import android.widget.EditText;
 import com.honeydo5.honeydo.R;
 
 import org.json.JSONObject;
-<<<<<<< HEAD
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-
 
 public class LoginScreen extends AppCompatActivity {
-
-<<<<<<< HEAD
-<<<<<<< HEAD
     private EditText email, password;
     private Button loginBtn, newBtn;
     private TextView message;
-=======
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
     public JSONObject dummy;
 
     private EditText email, password;
     private Button loginBtn, newBtn;
-<<<<<<< HEAD
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         loginBtn = findViewById(R.id.login);
         newBtn = findViewById(R.id.signup);
         message = findViewById(R.id.message);
-=======
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-        getJSON();
-
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-
-        loginBtn = (Button)findViewById(R.id.login);
-        newBtn = (Button)findViewById(R.id.signup);
-<<<<<<< HEAD
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
 
         loginBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -91,10 +56,7 @@ public class LoginScreen extends AppCompatActivity {
         });
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    private void attemptLogin()
-    {
+    private void attemptLogin() {
         Log.d("DEBUG", "Trying to login.");
 
         // TODO use JSON (the backend is not using JSON!)
@@ -107,19 +69,18 @@ public class LoginScreen extends AppCompatActivity {
                         // TODO this is ugly, talk to backend...
                         Log.d("DEBUG", response);
 
-                        switch(response)
-                        {
-                            case "logged in" :
+                        switch (response) {
+                            case "logged in":
                                 message.setText("Logged in!");
                                 Log.d("DEBUG", "Logged in!");
                                 break;
 
-                            case "you logged in already" :
+                            case "you logged in already":
                                 message.setText("You are already logged in.");
                                 Log.d("DEBUG", "You are already logged in.");
                                 break;
 
-                            case "wrong email/password" :
+                            case "wrong email/password":
                                 message.setText("Incorrect email or password.");
                                 Log.d("DEBUG", "Incorrect email or password.");
                                 break;
@@ -134,7 +95,7 @@ public class LoginScreen extends AppCompatActivity {
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Log.e("ERROR","Something happened in the way of heaven:" + error.getMessage());
+                Log.e("ERROR", "Something happened in the way of heaven:" + error.getMessage());
             }
         }) {
             @Override
@@ -149,33 +110,6 @@ public class LoginScreen extends AppCompatActivity {
         };
 
         AppController.getInstance().addToRequestQueue(loginRequest);
-=======
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-    private void getJSON(){
-        String dummyStr = "{\"user\":\"test\",\"password\":\"admin\"}";
-        try {
-            dummy = new JSONObject(dummyStr);
-        } catch (Throwable t)
-        {
-
-        }
-
-    }
-
-    private void attemptLogin()
-    {
-        String emailStr = email.getText().toString();
-        String passStr = password.getText().toString();
-
-        Intent intent = new Intent(this, MainScreen.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-        startActivity(intent);
-        this.finish();
-<<<<<<< HEAD
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
-=======
->>>>>>> 3a0d4b3d5629e846e944ce02d0bf84e95ee7d4af
     }
 }
 
