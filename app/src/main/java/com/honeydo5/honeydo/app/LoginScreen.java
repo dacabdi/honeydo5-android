e82e9018c05feb8ac51db019be0d5d32d111357b
dacabdi <dacabdi89@ufl.edu>
2018-03-14 11:30:55 -0400

    Created api-connector-login branch to work and test API polling with JSON

--- app/src/main/java/com/honeydo5/honeydo/app/LoginScreen.java
+++ app/src/main/java/com/honeydo5/honeydo/app/LoginScreen.java
@@ -1,5 +1,6 @@
 package com.honeydo5.honeydo.app;
 
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.honeydo5.honeydo.R;
 
 import android.os.Bundle;
@@ -10,30 +11,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.Request;
 import com.android.volley.VolleyError;
 import com.android.volley.Response;
 
-import java.util.Map;
-import java.util.HashMap;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-
-import com.honeydo5.honeydo.R;
-
 import org.json.JSONObject;
+import org.json.JSONException;
+
 
 public class LoginScreen extends AppCompatActivity {
     private EditText email, password;
     private Button loginBtn, newBtn;
     private TextView message;
-    public JSONObject dummy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,37 +43,38 @@ public class LoginScreen extends AppCompatActivity {
     }
 
     private void attemptLogin() {
-        StringRequest loginRequest = new StringRequest(
-                Request.Method.POST, AppController.defaultBaseUrl + "/login",
-                new Response.Listener<String>() {
+
+        JSONObject postMessage = new JSONObject();
+        try {
+            postMessage.put("email", email.getText());
+            postMessage.put("password", password.getText());
+        } catch (JSONException e) {
+            // TODO: do something about this exception, log it or something like that...
+        }
+
+        JsonObjectRequest loginReq = new JsonObjectRequest(
+                Request.Method.POST,
+                AppController.defaultBaseUrl + "/login",
+                postMessage,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onResponse(String response) {
+                    public void onResponse(JSONObject response) {
+                        Log.d("JSON-RESPONSE-LOGIN", response.toString());
 
-
-                        /*Context context = getApplicationContext();
-                        Intent intent = new Intent(context, MainScreen.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
-                        startActivity(intent);
-                        context.finish();*/
+                    /*Context context = getApplicationContext();
+                    Intent intent = new Intent(context, MainScreen.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_TASK_ON_HOME);
+                    startActivity(intent);
+                    context.finish();*/
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e("ERROR", "Something happened in the way of heaven:" + error.getMessage());
             }
-        }) {
-            @Override
-            protected Map<String, String> getParams() {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("email", email.getText().toString());
-                params.put("password", password.getText().toString());
+        });
 
-                Log.d("DEBUG", params.toString());
-                return params;
-            }
-        };
-
-        AppController.getInstance().addToRequestQueue(loginRequest);
+        AppController.getInstance().addToRequestQueue(loginReq);
     }
 }
 
