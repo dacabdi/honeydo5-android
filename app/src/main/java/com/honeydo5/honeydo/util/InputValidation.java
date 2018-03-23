282cfc4de8718f0a8f002257980cb3759d9bbc7e
dacabdi <dacabdi89@ufl.edu>
2018-03-23 16:41:50 -0400

    Added InputValidation class, with static methods to validate common fields

---
+++ app/src/main/java/com/honeydo5/honeydo/util/InputValidation.java
@@ -0,0 +1,26 @@
+package com.honeydo5.honeydo.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidation {
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String USERNAME_PATTERN = "[a-zA-Z0-9\\._\\-]{3,}"; //does not allow spaces
+
+    public static boolean validateEmail(String email){
+        return match(EMAIL_PATTERN, email);
+    }
+
+    public static boolean validateUsername(String username){
+        return match(USERNAME_PATTERN, username);
+    }
+
+    private static boolean match(String pattern, String str){
+        Pattern pattern;
+        Matcher matcher;
+        pattern = Pattern.compile(pattern);
+        matcher = pattern.matcher(str);
+
+        return matcher.matches();
+    }
+}
