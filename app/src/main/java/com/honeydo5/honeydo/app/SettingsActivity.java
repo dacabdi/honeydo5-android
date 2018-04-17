8a3cf6ec60421461ba942c36dfd0f1fe97cb6d10
Aaron Chambers <aaron.chambers81@gmail.com>
2018-04-17 19:18:19 -0400

    added preference screen, redid entire preference screen, added string vals for preference screen, added TODO for David on mute notification

--- app/src/main/java/com/honeydo5/honeydo/app/SettingsActivity.java
+++ app/src/main/java/com/honeydo5/honeydo/app/SettingsActivity.java
@@ -3,7 +3,9 @@ package com.honeydo5.honeydo.app;
 import com.honeydo5.honeydo.R;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
@@ -16,6 +18,15 @@ public class SettingsActivity extends PreferenceActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefFragment()).commit();
+    }
+
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals("pref_mute")) {
+            // Set summary to be the user-description for the selected value
+            if(sharedPreferences.getBoolean("pref_mute", false)) {
+                // TODO: David put the mute thing here
+            }
+        }
     }
 
     public static class PrefFragment extends PreferenceFragment {
